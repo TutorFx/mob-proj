abf91562bddfc4bfd8f121284589481fc520f5e4
TutorFx <gabriel_serejo@live.com>
2023-06-21 17:06:36 -0300

    :bug: Fix the redirecting glitch

--- src/components/ui/store-nav.vue
+++ src/components/ui/store-nav.vue
@@ -1,8 +1,7 @@
 <template>
   <ui-nav>
     <template #default>
-      <nuxt-link :to="{ name: 'loja-slug' }" class="btn btn-ghost normal-case font-black text-xl">{{ data?.name
-      }}</nuxt-link>
+      <nuxt-link :to="{ name: 'loja-slug', params: { slug } }" class="btn btn-ghost normal-case font-black text-xl">{{ data?.name }}</nuxt-link>
     </template>
     <template #end>
       <ui-cart-icon />
@@ -17,14 +16,18 @@
         <ul tabindex="0"
           class="menu menu-compact dropdown-content mt-3 p-2 shadow-2xl bg-base-100 rounded-box w-52 backdrop-blur-md bg-primary-content/60">
           <li>
-            <nuxt-link :to="{ name: 'dashboard' }" class="justify-between">
+            <nuxt-link :to="{ name: 'dashboard' }">
+              <Icon name="ic:outline-manage-accounts" />
               Perfil
-              <span class="badge">New</span>
             </nuxt-link>
           </li>
-          <li><nuxt-link :to="{ name: 'carteira' }">Carteira</nuxt-link></li>
+          <li><nuxt-link :to="{ name: 'carteira' }">
+              <Icon name="ic:outline-wallet" />Carteira
+            </nuxt-link></li>
           <li v-if="status === 'authenticated'">
-            <a @click.prevent="signOut()"> <Icon name="ic:baseline-output" /> Sair</a>
+            <a @click.prevent="signOut()">
+              <Icon name="ic:baseline-output" /> Sair
+            </a>
           </li>
         </ul>
       </div>
@@ -34,6 +37,7 @@
 
 <script setup lang="ts">
 defineProps<{ data: INav }>()
+const { slug } = useRoute().params
 const cart = useCart()
 const { signOut, status } = useAuth()
 </script>
