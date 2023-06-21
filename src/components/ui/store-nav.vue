7d3af87d03940d02d359778c13b5caefc380b0c8
TutorFx <gabriel_serejo@live.com>
2023-06-21 16:48:53 -0300

    :sparkles: Logout on profile button

--- src/components/ui/store-nav.vue
+++ src/components/ui/store-nav.vue
@@ -1,7 +1,8 @@
 <template>
   <ui-nav>
     <template #default>
-      <nuxt-link :to="{ name: 'loja-slug' }" class="btn btn-ghost normal-case font-black text-xl">{{ data?.name }}</nuxt-link>
+      <nuxt-link :to="{ name: 'loja-slug' }" class="btn btn-ghost normal-case font-black text-xl">{{ data?.name
+      }}</nuxt-link>
     </template>
     <template #end>
       <ui-cart-icon />
@@ -13,7 +14,8 @@
             </client-only>
           </div>
         </label>
-        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow-2xl bg-base-100 rounded-box w-52 backdrop-blur-md bg-primary-content/60">
+        <ul tabindex="0"
+          class="menu menu-compact dropdown-content mt-3 p-2 shadow-2xl bg-base-100 rounded-box w-52 backdrop-blur-md bg-primary-content/60">
           <li>
             <nuxt-link :to="{ name: 'dashboard' }" class="justify-between">
               Perfil
@@ -21,17 +23,17 @@
             </nuxt-link>
           </li>
           <li><nuxt-link :to="{ name: 'carteira' }">Carteira</nuxt-link></li>
-          <li>
-            <a>Sair</a>
+          <li v-if="status === 'authenticated'">
+            <a @click.prevent="signOut()"> <Icon name="ic:baseline-output" /> Sair</a>
           </li>
         </ul>
       </div>
     </template>
   </ui-nav>
-
 </template>
 
 <script setup lang="ts">
-  defineProps<{data: INav}>()
-  const cart = useCart()
+defineProps<{ data: INav }>()
+const cart = useCart()
+const { signOut, status } = useAuth()
 </script>
