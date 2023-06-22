d0af82f9a3e74fda80520de8559c33b3ec3d19fc
TutorFx <gabriel_serejo@live.com>
2023-06-22 15:02:23 -0300

    :bug: Fix the unmet cache to user session

--- src/components/ui/store-nav.vue
+++ src/components/ui/store-nav.vue
@@ -1,7 +1,8 @@
 <template>
   <ui-nav>
     <template #default>
-      <nuxt-link :to="{ name: 'loja-slug', params: { slug } }" class="btn btn-ghost normal-case font-black text-xl">{{ data?.name }}</nuxt-link>
+      <nuxt-link :to="{ name: 'loja-slug', params: { slug } }" class="btn btn-ghost normal-case font-black text-xl">{{
+        data?.name }}</nuxt-link>
     </template>
     <template #end>
       <ui-cart-icon />
@@ -15,20 +16,22 @@
         </label>
         <ul tabindex="0"
           class="menu menu-compact dropdown-content mt-3 p-2 shadow-2xl bg-base-100 rounded-box w-52 backdrop-blur-md bg-primary-content/60">
-          <li>
-            <nuxt-link :to="{ name: 'dashboard' }">
-              <Icon name="ic:outline-manage-accounts" />
-              Perfil
-            </nuxt-link>
-          </li>
-          <li><nuxt-link :to="{ name: 'carteira' }">
-              <Icon name="ic:outline-wallet" />Carteira
-            </nuxt-link></li>
-          <li v-if="status === 'authenticated'">
-            <a @click.prevent="signOut()">
-              <Icon name="ic:baseline-output" /> Sair
-            </a>
-          </li>
+          <client-only>
+            <li>
+              <nuxt-link :to="{ name: 'dashboard' }">
+                <Icon name="ic:outline-manage-accounts" />
+                Perfil
+              </nuxt-link>
+            </li>
+            <li><nuxt-link :to="{ name: 'carteira' }">
+                <Icon name="ic:outline-wallet" />Carteira
+              </nuxt-link></li>
+            <li v-if="status === 'authenticated'">
+              <a @click.prevent="signOut()">
+                <Icon name="ic:baseline-output" /> Sair
+              </a>
+            </li>
+          </client-only>
         </ul>
       </div>
     </template>
