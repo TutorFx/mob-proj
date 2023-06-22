f74c7d888659dd79f677e4b89776ff20585d3571
TutorFx <gabriel_serejo@live.com>
2023-06-21 22:16:29 -0300

    :sparkles: Introduce Product Sharing

--- src/pages/loja/[slug]/[product].vue
+++ src/pages/loja/[slug]/[product].vue
@@ -3,19 +3,13 @@
     <NuxtLoadingIndicator color="false" class="bg-primary" />
     <div class="container grid fill-screen grid-rows-[max-content_1fr]">
       <ui-store-nav :data="data" />
-      <ui-product-page :productdata="productdata" />
+      <ui-product-page :business-name="data?.name" :productdata="productdata" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const { slug, product } = useRoute().params
-/* if (!slug) {
-  throw createError({
-    statusCode: 404,
-    statusMessage: 'Product Not Found'
-  })
-} */
 
 const data = await $fetch(`/api/v1/business/${slug}`)
 /* if (error.value) {
