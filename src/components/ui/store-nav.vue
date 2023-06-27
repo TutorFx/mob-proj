37a60383a23024b2c7a24a0e0d156baff74ef57c
TutorFx <gabriel_serejo@live.com>
2023-06-27 11:10:07 -0300

    Revert "Merge pull request #3 from TutorFx/qa - Fix security issue with personal bubble redirect"
    This reverts commit 676bd440d3acf68b31def6b6818926336c9c7277, reversing
    changes made to e0ddb593647f36731845448ac0c820cb6bb9a964.

--- src/components/ui/store-nav.vue
+++ src/components/ui/store-nav.vue
@@ -6,7 +6,34 @@
     </template>
     <template #end>
       <ui-cart-icon />
-      <lazy-ui-personal-bubble />
+      <div class="dropdown dropdown-end">
+        <label tabindex="0" class="btn btn-ghost btn-md btn-circle avatar">
+          <div class="w-10 rounded-full">
+            <client-only>
+              <user-avatar />
+            </client-only>
+          </div>
+        </label>
+        <ul tabindex="0"
+          class="menu menu-compact dropdown-content mt-3 p-2 shadow-2xl bg-base-100 rounded-box w-52 backdrop-blur-md bg-primary-content/60">
+          <client-only>
+            <li>
+              <nuxt-link :to="{ name: 'dashboard' }">
+                <Icon name="ic:outline-manage-accounts" />
+                Perfil
+              </nuxt-link>
+            </li>
+            <li><nuxt-link :to="{ name: 'carteira' }">
+                <Icon name="ic:outline-wallet" />Carteira
+              </nuxt-link></li>
+            <li v-if="status === 'authenticated'">
+              <a @click.prevent="signOut()">
+                <Icon name="ic:baseline-output" /> Sair
+              </a>
+            </li>
+          </client-only>
+        </ul>
+      </div>
     </template>
   </ui-nav>
 </template>
@@ -15,4 +42,5 @@
 defineProps<{ data: INav }>()
 const { slug } = useRoute().params
 const cart = useCart()
+const { signOut, status } = useAuth()
 </script>
