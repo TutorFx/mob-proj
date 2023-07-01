f69e2a00656ed6d5f87a7f0df2095dbb6f087022
TutorFx <gabriel_serejo@live.com>
2023-07-01 11:52:36 -0300

    :bug: Fixes Authentication Issues

--- src/plugins/authentication.ts
+++ src/plugins/authentication.ts
@@ -1,33 +1,10 @@
 import { useAuthentication } from '@/composables/useAuthentication';
 
 export default defineNuxtPlugin(async (nuxt) => {
-  // @ts-expect-error
+  
   const login = useAuthentication(nuxt.$pinia);
   const route = useRoute();
 
-  addRouteMiddleware('auth', async (to, from) => {
-    if (process.client) {
-      if (!login.isAuthenticated) {
-        return navigateTo({
-          path: '/login', query: {
-            callback: encodeURI(to.fullPath)
-          }
-        });
-      }
-    }
-    // Perform Server side validation
-    if (process.server) {
-      try {
-        await $fetch('/api/v1/session')
-      } catch (e) {
-        return navigateTo({
-          path: '/login', query: {
-            callback: encodeURI(to.fullPath)
-          }
-        });
-      }
-    }
-  });
   // On unauthenticated request redirect and delete data
   watch(
     () => login.isAuthenticated,
