699fbef756181bf7f16eb470c412d62565a987ae
Gabriel Serejo <gabriel_serejo@live.com>
2023-10-07 18:30:06 -0300

    ✨ Introduce OpenAi completions (#26)
    * Start
    * fix...

---
+++ src/components/ui/inputs/text-area.vue
@@ -0,0 +1,45 @@
+<template>
+    <EditorContent :editor="editor" />
+</template>
+
+<script setup lang="ts">
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+const props = defineProps<{ modelValue: string }>()
+const emits = defineEmits<{
+    (e: 'update:modelValue', value: string): void,
+}>()
+
+const state = computed({
+    get() {
+        return props.modelValue
+    },
+    set(value) {
+        emits('update:modelValue', value)
+    }
+})
+
+const editor = useEditor({
+    content: state.value,
+    editorProps: {
+        attributes: {
+            class: 'w-full prose mx-auto focus:outline-none',
+        },
+    },
+    onUpdate: () => {
+        const html = editor.value?.getHTML()
+        if (!html) return state.value = '';
+        state.value = html
+    },
+    extensions: [
+        StarterKit,
+    ],
+})
+
+//watchEffect(() => editor.value?.commands.setContent(state.value))
+watch(state, () => {
+    editor.value?.commands.setContent(state.value)
+})
+
+</script>
