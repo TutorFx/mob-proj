ea830b9c4301041880997da737c0a77500801b66
TutorFx <gabriel_serejo@live.com>
2023-10-09 17:52:24 -0300

    Improve

--- src/server/utils/mailer.ts
+++ src/server/utils/mailer.ts
@@ -1,15 +1,17 @@
-import nodemailer from "nodemailer";
+import nodemailer, { createTransport, SendMailOptions } from "nodemailer";
 import path from "path";
 import hbs from "nodemailer-express-handlebars";
+import { string } from "zod";
 
-const config = useRuntimeConfig()
+const config = useRuntimeConfig();
 
-const transport = nodemailer.createTransport({
+const transport = createTransport({
+  // @ts-expect-error - it expects a undefined (???)
   host: config.brevo.SMTP_HOSTNAME,
   port: config.brevo.SMTP_PORT,
-  auth: { 
-    user: config.brevo.SMTP_USER, 
-    pass: config.brevo.SMTP_KEY 
+  auth: {
+    user: config.brevo.SMTP_USER,
+    pass: config.brevo.SMTP_KEY,
   },
 });
 
@@ -25,4 +27,63 @@ transport.use(
   })
 );
 
-export { transport };
+export enum templates {
+  recovery = "auth/recovery",
+}
+
+interface Email {
+  to: string;
+  from?: string;
+  subject?: string;
+  template?: templates;
+  context?: any;
+}
+
+interface RecoveryEmail extends Email {
+  context: {
+    token: string;
+  };
+}
+
+interface MailServiceContent extends Email {
+  transport: typeof transport;
+}
+
+interface MailService extends MailServiceContent {}
+class MailService {
+  constructor(email: Email) {
+    this.transport = transport;
+    this.to = email.to;
+    this.from = email.from;
+    this.subject = email.subject;
+    this.template = email.template;
+    this.context = email.context;
+  }
+  async sendMail() {
+    return await transport.sendMail(this);
+  }
+}
+
+class Recovery extends MailService {
+  from = "noreply@nuxa.io";
+  subject = "Recuperação de senha";
+  template = templates.recovery;
+  constructor(email: RecoveryEmail) {
+    super(email);
+    this.template = templates.recovery;
+  }
+}
+
+export const MailServices = {
+  Recovery,
+};
+
+/* 
+    const mail = new MailServices.Recovery({
+      to: "gabrieltfserejo@gmail.com",
+      context: {
+        token: "123",
+      }
+    });
+    const response = mail.sendMail();
+*/
