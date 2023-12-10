00a6184636454d84b85b019729f13155da2ef300
Gabriel Serejo <gabriel_serejo@live.com>
2023-12-10 18:29:44 +0000

    :bug: Fixes build errors regarding the last updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -128,11 +128,6 @@ export default defineNuxtConfig({
   nitro: {
     preset: "vercel",
     experimental: { openAPI: true },
-    storage: {
-      cache: {
-        data: { driver: "vercelKV" },
-      },
-    },
   },
 
   devtools: {
